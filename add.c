b3512d4943fe9d0ed5ca56ab64cdde4b52465363
colorcard <liwq6688@icloud.com>
2024-09-06 12:52:26 +0800

    update

---
+++ add.c
@@ -0,0 +1,14 @@
+//
+// Created by colorcard on 2024/9/6.
+//
+
+#include <stdio.h>
+
+int main(){
+    double x,y;
+
+    scanf("%lf %lf",&x,&y);
+    printf("%lf",x+y);
+
+    return 0;
+}
---
+++ int.c
@@ -0,0 +1,14 @@
+//
+// Created by colorcard on 2024/9/6.
+//
+
+#include <stdio.h>
+
+int main(){
+    int x,y,z;
+
+    scanf("%d %d %d",&x,&y,&z);
+    printf("%d",y);
+
+    return 0;
+}
