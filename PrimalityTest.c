2f21be8f0994eed1f89bce79ad5ea5f14c0a340d
colorcard <liwq6688@icloud.com>
2024-09-07 18:45:00 +0800

    fix

--- PrimalityTest.c
+++ PrimalityTest.c
@@ -3,9 +3,10 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-    int n,i=2;
+    int n,i;
 
     printf("请输入一个整数：\n");
     scanf("%d",&n);
@@ -15,16 +16,14 @@ int main(){
     }
     else{
 
-        while(n%i!=0){
-            i++;
+        for(i=2;i<=sqrt(n);i++){
+            if (n % i == 0) {
+                printf("这个数字不是素数。");
+                return 0;
+            }
         }
 
-        if(i==n){
             printf("这个数字是素数。");
-        }
-        else{
-            printf("这个数字不是素数。");
-        }
     }
 
     return 0;
