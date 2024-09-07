5c14d4526828acffceed1d6fb3cdb329ad84a386
colorcard <liwq6688@icloud.com>
2024-09-07 17:43:16 +0800

    修改了一个小错误

--- s_and_c.c
+++ s_and_c.c
@@ -11,9 +11,9 @@ int main() {
     printf("请输入第三个点的坐标：\n");
     scanf("%lf,%lf",&x3,&y3);
 
-    a=sqrt(pow(x1,2)+pow(y1,2)+pow(x2,2)+pow(y2,2));
-    b=sqrt(pow(x1,2)+pow(y1,2)+pow(x3,2)+pow(y3,2));
-    c=sqrt(pow(x3,2)+pow(y3,2)+pow(x2,2)+pow(y2,2));
+    a = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    b = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
+    c = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
 
     per=a+b+c;
     p=per/2;
