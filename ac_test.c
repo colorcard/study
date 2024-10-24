132b0693fbe64493bff0dbf71cb260981d13b7f5
colorcard <liwq6688@icloud.com>
2024-10-24 18:47:44 +0800

    update

--- ac_test.c
+++ ac_test.c
@@ -1,26 +1,10 @@
 #include "stdio.h"
 
 int main(){
-    int n,i,index=0,sum=0,a,avr;
-    printf("输入n：");
-    scanf("%d",&n);
 
-    for(i=0;i<n;i++){
-
-        scanf("%d",&a);
-
-        if(a<60){
-            index++;
-        }
-
-        sum=sum+a;
-
-        return 0;
-    }
-
-    avr=sum/n;
-
-    printf("平均分：%d\n不及格人数：%d",avr,index);
-
+    printf("Hello World!");
+    printf("hhhhh");
+    
+    return 0;
 
 }
