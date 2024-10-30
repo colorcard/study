01f97b04c39c7662af6ecbf18029b4bb94c76b1c
colorcard <liwq6688@icloud.com>
2024-10-30 22:28:05 +0800

    update

--- C_Algorithm/Recur_Fibonacci.c
+++ C_Algorithm/Recur_Fibonacci.c
@@ -4,11 +4,20 @@
 
 #include <stdio.h>
 
+int fibo(int n);
+
 int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d", fibo(n));
 
     return 0;
 }
 
-int fib(int n){
-
+int fibo(int n){
+    if(n==1||n==2) return n-1;
+    //结束条件
+    int res = fibo(n-1)+ fibo(n-2);
+    //普通递归，递归树，分治
+    return res;
 }
